68579c1fc4e266eabeec16976c3ee3e90359ccd9
Yana <averyanusha@gmail.com>
2026-03-24 16:58:28 +0000

    Finally sorted things with gh-pages branch, not so easy to deploy react in git pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'cv-application',
+  base: '/cv-application/',
   plugins: [
     react({
       babel: {
